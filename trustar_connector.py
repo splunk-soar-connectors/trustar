13f8d04e2dcc8ac723e677b217f079d8a9dde699
Brian Earle <brian@Brians-MacBook-Pro.local>
2017-10-13 15:03:16 -0700

    PAPP-3314 - CREST merge

--- trustar_connector.py
+++ trustar_connector.py
@@ -1,5 +1,5 @@
 # --
-# File: trustar/trustar_connector.py
+# File: trustar_connector.py
 #
 # Copyright (c) Phantom Cyber Corporation, 2017
 #
@@ -16,9 +16,6 @@
 import datetime
 import json
 import os
-import time
-import math
-import hashlib
 import socket
 import requests
 import pytz
@@ -681,8 +678,6 @@ class TrustarConnector(BaseConnector):
         enclave_ids = param.get(consts.TRUSTAR_JSON_ENCLAVE_IDS)
         time_discovered = param.get(consts.TRUSTAR_JSON_TIME_DISCOVERED)
         external_tracking_id = param.get(consts.TRUSTAR_JSON_TRACKING_ID)
-        # If api_version is not specified default will be "1.1"
-        api_version = param.get(consts.TRUSTAR_JSON_API_VERSION, '1.1')
 
         # Normalize timestamp
         report_time_began = self._normalize_timestamp(time_discovered)
@@ -717,19 +712,13 @@ class TrustarConnector(BaseConnector):
             config_enclave_ids_list = filter(
                 lambda x: x.strip(), [config_enclave_id.strip() for config_enclave_id in config_enclave_ids_list])
             # Return error if any of the enclave_id provided in action parameters is not configured in asset
-            if (set(enclave_id_list) - set(config_enclave_ids_list)):
+            if set(enclave_id_list) - set(config_enclave_ids_list):
                 return action_result.set_status(phantom.APP_ERROR, consts.TRUSTAR_UNKNOWN_ENCLAVE_ID)
             # Update request data
             submit_report_payload["enclaveIds"] = enclave_id_list
 
-        # Decide endpoint based on api version mentioned in asset configuration
-        # Update request data if api_version is 1.2 and external_tracking_id is provided
-        if api_version == "1.2":
-            endpoint = consts.TRUSTAR_SUBMIT_REPORT_ENDPOINT_1_2
-            if external_tracking_id:
-                submit_report_payload["incidentReport"]["externalTrackingId"] = external_tracking_id
-        else:
-            endpoint = consts.TRUSTAR_SUBMIT_REPORT_ENDPOINT
+        if external_tracking_id:
+            submit_report_payload["incidentReport"]["externalTrackingId"] = external_tracking_id
 
         # Generate token
         token_generation_status = self._generate_api_token(action_result)
@@ -739,7 +728,7 @@ class TrustarConnector(BaseConnector):
             return action_result.get_status()
 
         # Make REST call
-        resp_status, response = self._make_rest_call(endpoint, action_result, data=json.dumps(submit_report_payload),
+        resp_status, response = self._make_rest_call(consts.TRUSTAR_SUBMIT_REPORT_ENDPOINT, action_result, data=json.dumps(submit_report_payload),
                                                      method="post", headers={'Content-Type': 'application/json'})
 
         # Something went wrong
@@ -756,216 +745,6 @@ class TrustarConnector(BaseConnector):
         # Update summary data
         summary_data['report_id'] = response['reportId']
         summary_data['total_extracted_indicators'] = indicators_count
-
-        return action_result.set_status(phantom.APP_SUCCESS)
-
-    def _create_dict_hash(self, input_dict):
-        """ Function used to create hash of the given input dictionary.
-
-        :param input_dict: dictionary for which we need to generate hash
-        :return: MD5 hash
-        """
-
-        input_dict_str = None
-
-        if not input_dict:
-            return None
-
-        try:
-            input_dict_str = json.dumps(input_dict, sort_keys=True)
-        except Exception as e:
-            print str(e)
-            self.debug_print('Handled exception in _create_dict_hash', e)
-            return None
-
-        return hashlib.md5(input_dict_str).hexdigest()
-
-    def _create_artifact(self, artifact_name, cef, cef_types, container_id, artifact_created, artifact_count):
-        """ Create artifact using the data provided.
-
-        :param artifact_name: name of the artifact
-        :param cef: dictionary containing artifact's cef and its value
-        :param cef_types: dictionary containing artifact's cef and its contains
-        :param container_id: container id in which we need to ingest artifacts
-        :param artifact_created: created artifacts count
-        :param artifact_count: expected artifacts count
-        :return: status success
-        """
-
-        # Prepare artifact dictionary
-        artifact = {'name': artifact_name, "description": "TruSTAR artifacts", "cef_types": cef_types, 'cef': cef,
-                    'container_id': container_id}
-
-        # If its the last artifact during ingestion update it to set 'run_automation' key
-        if artifact_created == artifact_count:
-            artifact.update({'run_automation': True})
-
-        # The source_data_identifier should be created after all the keys have been set.
-        artifact['source_data_identifier'] = self._create_dict_hash(artifact)
-
-        # Save artifact
-        artifact_return_value, status_string, artifact_id = self.save_artifact(artifact)
-
-        # Something went wrong while creating artifact
-        if phantom.is_fail(artifact_return_value):
-            self.debug_print("Error while creating {}".format(artifact_name), artifact)
-
-        return phantom.APP_SUCCESS
-
-    def _ingest_data(self, iocs, artifact_count):
-        """ This method creates container from the provided data and prepare data to create artifacts.
-
-        :param iocs: data dictionary from which we need to create artifacts
-        :param artifact_count: expected artifacts count that should be ingested
-        :return: status success
-        """
-
-        try:
-            request_time = time.strftime("%Y/%m/%d %H:%M:%S", time.localtime(iocs['queryDate'] / 1000))
-        except:
-            self.save_progress(consts.TRUSTAR_INGESTION_REQUEST_TIME_ERROR)
-            self.debug_print(consts.TRUSTAR_INGESTION_REQUEST_TIME_ERROR)
-            return
-
-        # Prepare container dictionary
-        container = dict(
-            name='TRUSTAR_IOCS' + '-' + str(request_time) + '-' + str(iocs['intervalSize']),
-            description='TRUSTAR_IOCS', data=iocs,
-            source_data_identifier='TRUSTAR_IOCS' + '-' + str(request_time) + '-' + str(iocs['intervalSize'])
-        )
-
-        # Save container
-        container_return_value, response, container_id = self.save_container(container)
-
-        # Something went wrong while creating container
-        if phantom.is_fail(container_return_value):
-            self.save_progress("Error while creating container")
-            self.debug_print("Error while creating container", dump_object=container)
-            return
-
-        # Counter to maintain the count of artifacts created
-        artifacts_created = 0
-        # Dictionary object containing information about artifact name, its corresponding cef name and contains
-        cef_mapping = {
-            # File artifacts
-            "SHA256": {"artifact_name": "File Artifact", "cef_name": "fileHashSha256", "cef_contains": ["sha256"]},
-            "SHA1": {"artifact_name": "File Artifact", "cef_name": "fileHashSha1", "cef_contains": ["sha1"]},
-            "MD5": {"artifact_name": "File Artifact", "cef_name": "fileHashMd5", "cef_contains": ["md5"]},
-            "SOFTWARE": {"artifact_name": "File Artifact", "cef_name": "filePath",
-                         "cef_contains": ["file name", "file path"]},
-            # Email artifact
-            "EMAIL_ADDRESS": {"artifact_name": "Email Artifact", "cef_name": "email", "cef_contains": ["email"]},
-
-            # IP artifacts
-            "IP": {"artifact_name": "IP Artifact", "cef_name": "destinationAddress", "cef_contains": ["ip"]},
-            "CIDR_BLOCK": {"artifact_name": "IP Artifact", "cef_name": "destinationAddress", "cef_contains": ["ip"]},
-
-            # Domain artifact
-            "BITCOIN_ADDRESS": {"artifact_name": "Bitcoin Address Artifact", "cef_name": "bitcoinAddress",
-                                "cef_contains": ["trustar bitcoin address"]},
-
-            # URL artifact
-            "URL": {"artifact_name": "URL Artifact", "cef_name": "requestURL", "cef_contains": ["url"]},
-
-            # Malware artifact
-            "MALWARE": {"artifact_name": "Malware Artifact", "cef_name": "malware",
-                        "cef_contains": ["trustar malware"]},
-
-            # CVE artifact
-            "CVE": {"artifact_name": "CVE Artifact", "cef_name": "cs3", "cef_contains": ["trustar cve number"]},
-
-            # Registry Key artifact
-            "REGISTRY_KEY": {"artifact_name": "Registry Key Artifact", "cef_name": "registryKey",
-                             "cef_contains": ["trustar registry key"]}
-        }
-
-        # Iterate over cef_mapping to get the required information to ingest artifacts
-        for ioc_key in cef_mapping:
-            cef = dict()
-            cef_types = dict()
-            # Fetch details of ioc_key from cef_mapping
-            cef_details = cef_mapping[ioc_key]
-            # Fetch artifact_name, cef_name and cef_contains of ioc_key from cef_mapping
-            cef_name = cef_details["cef_name"]
-            cef_contains = cef_details["cef_contains"]
-            artifact_name = cef_details["artifact_name"]
-            # Get list of all values of the ioc from the iocs parameter passed in the function
-            ioc_values = iocs['indicators'].get(ioc_key)
-            if ioc_values:
-                # Ingest each value of ioc as a separate artifact
-                for ioc_value in ioc_values:
-                    cef[cef_name] = ioc_value
-                    cef_types[cef_name] = cef_contains
-                    # Increment the count of created artifact
-                    artifacts_created += 1
-                    # Create artifact
-                    self._create_artifact(artifact_name, cef, cef_types, container_id, artifacts_created,
-                                          artifact_count)
-
-        return phantom.APP_SUCCESS
-
-    def _on_poll(self, param):
-        """ This method ingests the latest indicators that were recently shared on the TruSTAR Station
-         (caller's enclave(s) and community reports) or collected from the open source.
-
-        :param param: dictionary of input parameters
-        :return: status success/failure
-        """
-
-        action_result = self.add_action_result(ActionResult(dict(param)))
-
-        # Get action parameters
-        start_time = param.get(phantom.APP_JSON_START_TIME)
-        end_time = param.get(phantom.APP_JSON_END_TIME)
-
-        # Generate token
-        token_generation_status = self._generate_api_token(action_result)
-
-        # Something went wrong while generating token
-        if phantom.is_fail(token_generation_status):
-            return action_result.get_status()
-
-        self.save_progress("Fetching latest IOCs")
-
-        # POLL NOW
-        if self.is_poll_now():
-            self.save_progress("Ignoring Source ID")
-            self.save_progress("Ignoring Maximum containers and Maximum artifacts count")
-            # Make REST call
-            resp_status, response = self._make_rest_call(consts.TRUSTAR_LATEST_IOC_ENDPOINT, action_result)
-
-        # First scheduled ingestion
-        elif self._app_state.get('first_run', True):
-            self._app_state['first_run'] = False
-            # Make REST call
-            resp_status, response = self._make_rest_call(consts.TRUSTAR_LATEST_IOC_ENDPOINT, action_result)
-
-        # Scheduled ingestion
-        else:
-            # Convert epoch milliseconds to seconds
-            start_time /= 1000
-            end_time /= 1000
-            # Find diff between start & end time, convert it into hours and consider ceil value as interval
-            diff_in_seconds = end_time - start_time
-            interval = int(math.ceil(diff_in_seconds / 3600.0))
-            # Prepare request params
-            params = {'intervalSize': interval}
-            # Make REST call
-            resp_status, response = self._make_rest_call(consts.TRUSTAR_LATEST_IOC_ENDPOINT, action_result,
-                                                         params=params)
-
-        # Something went wrong while fetching latest indicators
-        if phantom.is_fail(resp_status):
-            return action_result.get_status()
-
-        # Get dictionary of indicators from response obtained
-        indicators = response.get('indicators')
-        if indicators:
-            # Count total artifacts
-            total_artifacts = 0
-            for indicator in indicators.keys():
-                total_artifacts += len(indicators[indicator])
-            self._ingest_data(response, total_artifacts)
 
         return action_result.set_status(phantom.APP_SUCCESS)
 
@@ -989,24 +768,25 @@ class TrustarConnector(BaseConnector):
         """
 
         # Dictionary mapping each action with its corresponding actions
-        action_mapping = {'test_asset_connectivity': self._test_asset_connectivity,
-                          'hunt_ip': self._hunt_ip,
-                          'hunt_url': self._hunt_url,
-                          'hunt_email': self._hunt_email,
-                          'hunt_file': self._hunt_file,
-                          'hunt_cve': self._hunt_cve,
-                          'hunt_malware': self._hunt_malware,
-                          'hunt_registry_key': self._hunt_registry_key,
-                          'hunt_bitcoin_address': self._hunt_bitcoin_address,
-                          'get_report': self._get_report,
-                          'submit_report': self._submit_report,
-                          'on_poll': self._on_poll}
+        action_mapping = {
+            'test_asset_connectivity': self._test_asset_connectivity,
+            'hunt_ip': self._hunt_ip,
+            'hunt_url': self._hunt_url,
+            'hunt_email': self._hunt_email,
+            'hunt_file': self._hunt_file,
+            'hunt_cve': self._hunt_cve,
+            'hunt_malware': self._hunt_malware,
+            'hunt_registry_key': self._hunt_registry_key,
+            'hunt_bitcoin_address': self._hunt_bitcoin_address,
+            'get_report': self._get_report,
+            'submit_report': self._submit_report
+        }
 
         action = self.get_action_identifier()
 
         try:
             run_action = action_mapping[action]
-        except:
+        except Exception:
             raise ValueError("action {action} is not supported".format(action=action))
 
         return run_action(param)
